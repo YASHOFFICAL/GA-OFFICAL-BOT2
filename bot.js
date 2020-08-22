fc57b484f20ecef4d4469ddf2ad6c5f4caebde6a
YASHOFFICAL <70049117+YASHOFFICAL@users.noreply.github.com>
2020-08-22 11:59:26 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,9 +7,9 @@ bot.on('message', message =>{
     // Variables
     var sender = message.author; //The person who sent the message
     var msg = message.content;
-    var prefix = ">" //The text before commands, you can put anything that you prefer
+    var prefix = "/" //The text before commands, you can put anything that you prefer
 
-    if(message.author.id != "381443488392413184" && message.channel.id === "741901544537980978"){
+    if(message.author.id != "704620271553740823" && message.channel.id === "741901544537980978"){
         if(msg.startsWith('/', 0)){
             if(msg === prefix + "HI" && message.channel.id === "741901544537980978"){
                 message.channel.send('HELLO') // Sends a message to the channel, with the content
