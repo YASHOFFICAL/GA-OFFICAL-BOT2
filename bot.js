f4bf26f45e61b5f23f6c9d5a4d27e3b1c514eb41
YASHOFFICAL <70049117+YASHOFFICAL@users.noreply.github.com>
2020-08-22 12:11:58 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,11 +7,11 @@ bot.on('message', message =>{
     // Variables
     var sender = message.author; //The person who sent the message
     var msg = message.content;
-    var prefix = "/" //The text before commands, you can put anything that you prefer
+    var prefix = ">" //The text before commands, you can put anything that you prefer
 
     if(message.author.id != "704620271553740823" && message.channel.id === "741901544537980978"){
-        if(msg.startsWith('/', 0)){
-            if(msg === prefix + "HI" && message.channel.id === "741901544537980978"){
+        if(msg.startsWith('>', 0)){
+            if(msg === prefix + "Hi" && message.channel.id === "741901544537980978"){
                 message.channel.send('HELLO') // Sends a message to the channel, with the content
             }
             else{
@@ -20,7 +20,7 @@ bot.on('message', message =>{
         }
         else{
             message.delete();
-            message.channel.send(sender + " this bot only accepts commands which starts with '/' dot.");
+            message.channel.send(sender + " this bot only accepts commands which starts with '>' dot.");
         }
     }
 });
